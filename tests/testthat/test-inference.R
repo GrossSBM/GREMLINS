0aa5f2d3e83533ec5c84b2f05debc6561ecd90ed
barbillon <pierre.barbillon@agroparistech.fr>
2019-08-27 08:58:09 +0200

    ajout test inference

---
+++ tests/testthat/test-inference.R
@@ -0,0 +1,31 @@
+set.seed(123)
+v_K <- c(2,2)
+n_FG <- 2
+list_pi <- vector("list", 2);
+list_pi[[1]] <- c(0.5,.5); list_pi[[2]] <- c(0.6,0.4)
+E  = rbind(c(1,2),c(2,2))
+v_distrib <- c('bernoulli','poisson')
+typeInter <- c( "inc", "adj" )
+list_theta <- list()
+list_theta[[1]] <- matrix(rbeta(v_K[E[1,1]] * v_K[E[1,2]],1.5,1.5 ),nrow = v_K[E[1,1]], ncol = v_K[E[1,2]])
+list_theta[[2]] <- matrix(rgamma(v_K[E[2,1]] * v_K[E[2,2]],7.5,1 ),nrow = v_K[E[2,1]], ncol = v_K[E[2,2]])
+v_NQ = c(50,40)
+dataSim <-  rMBM(v_NQ ,E , typeInter, v_distrib, list_pi, list_theta, seed=NULL, namesFG= c('A','B'),keepClassif = TRUE)
+list_Net <- dataSim$list_Net
+res <- multipartiteBM(list_Net, v_distrib = c("bernoulli","poisson"), namesFG = NULL, v_Kmin = 1,v_Kmax = 5,v_Kinit = NULL,verbose = TRUE, save=FALSE, maxiterVE = NULL)
+
+res$fittedModel[[1]]
+
+library(aricode)
+
+# ARI(res$fittedModel[[1]]$paramEstim$Z[[1]],dataSim$classif[[1]])
+# ARI(res$fittedModel[[1]]$paramEstim$Z[[2]],dataSim$classif[[2]])
+
+test_that("correct clustering 1st FG", {
+  expect_gt(ARI(res$fittedModel[[1]]$paramEstim$Z[[1]],dataSim$classif[[1]]),.4)
+})
+
+
+test_that("correct clustering 2nd FG", {
+  expect_gt(ARI(res$fittedModel[[1]]$paramEstim$Z[[2]],dataSim$classif[[2]]),.4)
+})
