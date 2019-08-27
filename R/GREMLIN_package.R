7c32f2ad3bb4d6151527d9c3b662d80c07ed8ea4
barbillon <pierre.barbillon@agroparistech.fr>
2019-08-27 15:00:16 +0200

    dependance aricode

--- R/GREMLIN_package.R
+++ R/GREMLIN_package.R
@@ -2,7 +2,7 @@
 #'
 #' All required packages
 #' @importFrom R6 R6Class
-#' @import parallel igraph blockmodels
+#' @import parallel igraph blockmodels aricode
 #' @docType package
 #' @name GREMLIN
 NULL
